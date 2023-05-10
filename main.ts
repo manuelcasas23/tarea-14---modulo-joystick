23f4f5bf22c932bdfbfc20de3e9627dd54cefd52
manuelcasas <128051762+manuelcasas23@users.noreply.github.com>
2023-05-10 17:50:23 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,26 @@
+let marcha = 0
 let giro = 0
+basic.forever(function () {
+    if (marcha < 400) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    } else if (marcha > 700) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+    } else if (giro < 400) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+    } else if (giro > 700) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+    } else if (!(input.pinIsPressed(TouchPin.P0))) {
+        music.playMelody("C5 - - - - - - - ", 1000)
+    } else {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+})
+basic.forever(function () {
+    marcha = pins.analogReadPin(AnalogPin.P1)
+})
 basic.forever(function () {
     giro = pins.analogReadPin(AnalogPin.P2)
 })
 basic.forever(function () {
-    if (giro < 400) {
-        maqueen.motorStop(maqueen.Motors.M2)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
-    } else if (giro > 700) {
-        maqueen.motorStop(maqueen.Motors.M1)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
-    } else {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 250)
-    }
+    basic.showIcon(IconNames.Butterfly)
 })
