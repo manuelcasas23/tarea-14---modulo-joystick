41b0e4302dacca805b9d188d79abf47ff0bad4b1
manuelcasas <128051762+manuelcasas23@users.noreply.github.com>
2023-05-10 17:51:30 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,25 @@
 basic.forever(function () {
-    basic.clearScreen()
     led.plot(pins.map(
-    pins.analogReadPin(AnalogPin.P0),
+    pins.analogReadPin(AnalogPin.P2),
     0,
     1023,
     0,
     5
     ), pins.map(
-    pins.analogReadPin(AnalogPin.P1),
+    pins.analogReadPin(AnalogPin.P3),
     0,
     1023,
     5,
     0
     ))
-    if (!(input.pinIsPressed(TouchPin.P2))) {
+    servos.P0.setAngle(pins.map(
+    pins.analogReadPin(AnalogPin.P2),
+    0,
+    1023,
+    0,
+    180
+    ))
+    if (!(input.pinIsPressed(TouchPin.P1))) {
         music.playTone(659, music.beat(BeatFraction.Quarter))
     }
 })
